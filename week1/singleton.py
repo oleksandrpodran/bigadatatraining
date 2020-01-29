4636fde3b200695a150d44a6f33dd203c3d92008
oleksandrpodran <alexpodran@gmail.com>
2020-01-29 12:33:00 +0200

    Fixed Singleton

---
+++ week1/singleton.py
@@ -0,0 +1,14 @@
+class Singleton(object):
+    _instance = None
+    
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = object.__new__(cls, *args, **kwargs)
+        return cls._instance
+
+
+s = Singleton()
+print("Object created", s)
+s1 = Singleton()
+print("Object created", s1)
+
